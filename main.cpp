c8fee5e85e034d8f1440c47ac4ed67a6298004ff
ku-rihara <k023g0027@g.neec.ac.jp>
2024-07-04 13:54:56 +0900

    MT3_03_02

--- main.cpp
+++ main.cpp
@@ -10,54 +10,10 @@
 
 const char kWindowTitle[] = "LE2A_11_クリハラ_ケイタ_タイトル";
 
-struct Sphere {
-	Vector3 center;
-	float radius;
-};
 
-struct Segment {
-	Vector3 origin;
-	Vector3 diff;
-};
-
-struct Ray {
-	Vector3 origin;
-	Vector3 diff;
-};
-
-struct Line {
-	Vector3 origin;
-	Vector3 diff;
-};
-
-struct AABB {
-	Vector3 min;
-	Vector3 max;
-};
-
-struct OBB {
-	Vector3 center;
-	Vector3 orientations[3];
-	Vector3 size;
-};
 
 static const int kWindowWidth = 1280;
 static const int kWindowHeight = 720;
-
-
-void DrawGrid(const Matrix4x4& viewProjectionMatrix, const Matrix4x4& viewportMatrix);
-
-Vector3 Perpendicular(const Vector3& vector);
-
-bool IsCollision(const AABB& aabb1, const AABB& aabb2);
-
-
-float Clamp(float n, float min, float max);
-size_t Clamp(size_t n, size_t min, size_t max);
-Vector3 Lerp(const Vector3& start, const Vector3& end, float t);
-
-
-void DrawSphere(const Sphere& sphere, const Matrix4x4& viewProjectionMatrix, const Matrix4x4& viewprtMatrix, uint32_t color);
 
 
 // Windowsアプリでのエントリーポイント(main関数)
@@ -69,21 +25,16 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	Vector3 cameraTranslate{ 0.0f,0.0f,-6.49f };
 	Vector3 cameraRotate{ 0.26f,0.0f,0.0f };
 
-	Vector3 translates[3] = {
-		{0.2f,1.0f,0.0f},
-		{0.4f,0.0f,0.0f},
-		{0.3f,0.0f,0.0f},
-	};
-	Vector3 rotates[3] = {
-		{0.0f,0.0f,-6.8f},
-		{0.0f,0.0f,-1.4f},
-		{0.0f,0.0f,0.0f},
-	};
-	Vector3 scales[3] = {
-		{1.0f,1.0f,1.0f},
-		{1.0f,1.0f,1.0f},
-		{1.0f,1.0f,1.0f},
-	};
+	Vector3 a{ 0.2f,1.0f,0.0f };
+	Vector3 b{ 2.4f,3.1f,1.2f };
+	Vector3 c = a + b;
+	Vector3 d = a - b;
+	Vector3 e = a * 2.4f;
+	Vector3 rotate{ 0.4f,1.43f,-0.8f };
+	Matrix4x4 rotateXMatrix = Matrix4x4::MakeRotateXMatrix(rotate.x);
+	Matrix4x4 rotateYMatrix = Matrix4x4::MakeRotateYMatrix(rotate.y);
+	Matrix4x4 rotateZMatrix = Matrix4x4::MakeRotateZMatrix(rotate.z);
+	Matrix4x4 rotateMatrix = rotateXMatrix * rotateYMatrix * rotateZMatrix;
 
 	// キー入力結果を受け取る箱
 	char keys[256] = { 0 };
@@ -102,68 +53,20 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓更新処理ここから
 		///
 		ImGui::Begin("Window");
-		ImGui::DragFloat3("CameraTranslate", &cameraTranslate.x, 0.01f);
-		Matrix4x4 camelaMatrix = Matrix4x4::MakeAffineMatrix({ 1.0f,1.0f,1.0f }, {}, cameraTranslate);
-		camelaMatrix = camelaMatrix * Matrix4x4::MakeRotateXMatrix(cameraRotate.x);
-		camelaMatrix = camelaMatrix * Matrix4x4::MakeRotateYMatrix(cameraRotate.y);
-		camelaMatrix = camelaMatrix * Matrix4x4::MakeRotateZMatrix(cameraRotate.z);
-		ImGui::DragFloat3("CameraRotate", &cameraRotate.x, 0.01f);
+		ImGui::Text("c:%f,%f,%f", c.x, c.y, c.z);
+		ImGui::Text("d:%f,%f,%f", d.x,d.y, d.z);
+		ImGui::Text("e:%f,%f,%f", e.x, e.y, e.z);
+		ImGui::Text(
+			"matrix:\n%f,%f,%f,%f\n%f,%f,%f,%f\n%f,%f,%f,%f\n%f,%f,%f,%f\n",
+			rotateMatrix.m[0][0], rotateMatrix.m[0][1], rotateMatrix.m[0][2],
+			rotateMatrix.m[0][3], rotateMatrix.m[1][0], rotateMatrix.m[1][1],
+			rotateMatrix.m[1][2], rotateMatrix.m[1][3], rotateMatrix.m[2][0],
+			rotateMatrix.m[2][1], rotateMatrix.m[2][2], rotateMatrix.m[2][3],
+			rotateMatrix.m[3][0], rotateMatrix.m[3][1], rotateMatrix.m[3][2],
+			rotateMatrix.m[3][3]);
 		ImGui::End();
 
-		ImGui::Begin("ControlPoint");
-		ImGui::DragFloat3("translates[0]", &translates[0].x, 0.01f);
-		ImGui::DragFloat3("rotates[0]", &rotates[0].x, 0.01f);
-		ImGui::DragFloat3("sacles[0]", &scales[0].x, 0.01f);
-		ImGui::DragFloat3("translates[1]", &translates[1].x, 0.01f);
-		ImGui::DragFloat3("rotates[1]", &rotates[1].x, 0.01f);
-		ImGui::DragFloat3("sacles[1]", &scales[1].x, 0.01f); 
-		ImGui::DragFloat3("translates[2]", &translates[2].x, 0.01f);
-		ImGui::DragFloat3("rotates[2]", &rotates[2].x, 0.01f);
-		ImGui::DragFloat3("sacles[2]", &scales[2].x, 0.01f);
-		ImGui::End();
-
-		
 	
-
-		Matrix4x4 viewMatrix = Matrix4x4::Inverse(camelaMatrix);
-		Matrix4x4 projectionMatrix = Matrix4x4::MakePerspectiveFovMatrix(0.45f, float(kWindowWidth) / float(kWindowHeight), 0.1f, 100.0f);
-		Matrix4x4 ViewProjectionMatrix = viewMatrix * projectionMatrix;
-		Matrix4x4 viewportMatrix = Matrix4x4::MakeViewportMatrix(0, 0, float(kWindowWidth), float(kWindowHeight), 0.0f, 1.0f);
-
-		//肩
-		Matrix4x4 sholderTransformMatrix = Matrix4x4::MakeAffineMatrix(scales[0], rotates[0], translates[0]);
-		Sphere shoulder = { Vector3(sholderTransformMatrix.m[3][0],sholderTransformMatrix.m[3][1],sholderTransformMatrix.m[3][2]), 0.1f};
-		//肘
-		Matrix4x4 elbowTransformMatrix = Matrix4x4::MakeAffineMatrix(scales[1], rotates[1], translates[1])* sholderTransformMatrix;
-		Sphere elbow = { Vector3(elbowTransformMatrix.m[3][0], elbowTransformMatrix.m[3][1], elbowTransformMatrix.m[3][2]), 0.1f };
-		//手
-		Matrix4x4 handTransformMatrix = Matrix4x4::MakeAffineMatrix(scales[2], rotates[2], translates[2])* elbowTransformMatrix;
-		Sphere hand = { Vector3(handTransformMatrix.m[3][0], handTransformMatrix.m[3][1], handTransformMatrix.m[3][2]), 0.1f };
-		
-		Matrix4x4 sholderWvpMatrix = sholderTransformMatrix * ViewProjectionMatrix;
-		Matrix4x4 elbowWvpMatrix = elbowTransformMatrix * ViewProjectionMatrix;
-		Matrix4x4 handWvpMatrix = handTransformMatrix * ViewProjectionMatrix;
-		Vector3  sholderScreen = Matrix4x4::ScreenTransform(Vector3{}, sholderWvpMatrix, viewportMatrix);
-		Vector3 elbowScreen = Matrix4x4::ScreenTransform(Vector3{}, elbowWvpMatrix, viewportMatrix);
-		Vector3  handScreen = Matrix4x4::ScreenTransform(Vector3{}, handWvpMatrix, viewportMatrix);
-		///
-		/// ↑更新処理ここまで
-		///
-
-		///
-		/// ↓描画処理ここから
-		///
-		
-		DrawSphere(shoulder, ViewProjectionMatrix, viewportMatrix, RED);
-		DrawSphere(elbow, ViewProjectionMatrix, viewportMatrix, GREEN);
-		DrawSphere(hand, ViewProjectionMatrix, viewportMatrix, BLUE);
-	
-		Novice::DrawLine(int(sholderScreen.x), int(sholderScreen.y), int(elbowScreen.x), int(elbowScreen.y), WHITE);
-
-		Novice::DrawLine(int(elbowScreen.x), int(elbowScreen.y), int(handScreen.x), int(handScreen.y), WHITE);
-
-		DrawGrid(ViewProjectionMatrix, viewportMatrix);
-			
 		/// ↑描画処理ここまで
 		///
 
@@ -182,162 +85,4 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 }
 
 
-void DrawGrid(const Matrix4x4& viewProjectionMatrix, const Matrix4x4& viewportMatrix) {
-	const float kGridHalfWidth = 2.0f;//Gridの半分の幅
-	const uint32_t kSubdivision = 10;//分割数
-	const float kGridEvery = (kGridHalfWidth * 2.0f) / float(kSubdivision);//1つ分の長さ
-	Vector3 zLineStart;
-	Vector3 zLineEnd;
-	Vector3 xLineStart;
-	Vector3 xLineEnd;
-	//奥から手前への線を順々に引いていく
-	for (uint32_t xIndex = 0; xIndex <= kSubdivision; ++xIndex) {
-		//上の情報を使ってワールド座標系の始点と終点を求める
-		   // ワールド座標系でのグリッドの始点と終点を設定
-		zLineStart = Vector3(xIndex * kGridEvery - kGridHalfWidth, 0, kGridHalfWidth);
-		zLineEnd = Vector3(xIndex * kGridEvery - kGridHalfWidth, 0, -kGridHalfWidth);
-
-		//アフィン変換
-		Matrix4x4  zLineStartMatrix = Matrix4x4::MakeAffineMatrix(Vector3{ 1,1,1 }, Vector3{}, zLineStart);
-		Matrix4x4 zLineEndMatrix = Matrix4x4::MakeAffineMatrix(Vector3{ 1,1,1 }, Vector3{}, zLineEnd);
-		//wvpMatrix
-		Matrix4x4  zLineStartwvpMatrix = zLineStartMatrix * viewProjectionMatrix;
-		Matrix4x4  zLineEndwvpMatrix = zLineEndMatrix * viewProjectionMatrix;
-		//Screen変換
-		Vector3  zLineScreenStart = Matrix4x4::ScreenTransform(Vector3{}, zLineStartwvpMatrix, viewportMatrix);
-		Vector3  zLineScreenEnd = Matrix4x4::ScreenTransform(Vector3{}, zLineEndwvpMatrix, viewportMatrix);
-
-		if (xIndex == kSubdivision / 2) {
-			Novice::DrawLine(int(zLineScreenStart.x), int(zLineScreenStart.y), int(zLineScreenEnd.x), int(zLineScreenEnd.y), 0x000000FF);
-
-		}
-		else {
-			Novice::DrawLine(int(zLineScreenStart.x), int(zLineScreenStart.y), int(zLineScreenEnd.x), int(zLineScreenEnd.y), 0xAAAAAAFF);
-		}
-	}
-
-	for (uint32_t zIndex = 0; zIndex <= kSubdivision; ++zIndex) {
-		//上の情報を使ってワールド座標系の始点と終点を求める
-		   // ワールド座標系でのグリッドの始点と終点を設定
-		xLineStart = Vector3(-kGridHalfWidth, 0, zIndex * kGridEvery - kGridHalfWidth);
-		xLineEnd = Vector3(kGridHalfWidth, 0, zIndex * kGridEvery - kGridHalfWidth);
-
-		//アフィン変換
-		Matrix4x4 	xLineStartMatrix = Matrix4x4::MakeAffineMatrix(Vector3{ 1,1,1 }, Vector3{}, xLineStart);
-		Matrix4x4 xLineEndMatrix = Matrix4x4::MakeAffineMatrix(Vector3{ 1,1,1 }, Vector3{}, xLineEnd);
-		//wvpMatrix
-		Matrix4x4 	xLineStartwvpMatrix = xLineStartMatrix * viewProjectionMatrix;
-		Matrix4x4 xLineEndwvpMatrix = xLineEndMatrix * viewProjectionMatrix;
-		//Screen変換
-		Vector3 xLineScreenStart = Matrix4x4::ScreenTransform(Vector3{}, xLineStartwvpMatrix, viewportMatrix);
-		Vector3 xLineScreenEnd = Matrix4x4::ScreenTransform(Vector3{}, xLineEndwvpMatrix, viewportMatrix);
-		if (zIndex == kSubdivision / 2) {
-			Novice::DrawLine(int(xLineScreenStart.x), int(xLineScreenStart.y), int(xLineScreenEnd.x), int(xLineScreenEnd.y), 0x000000FF);
-		}
-		else {
-			Novice::DrawLine(int(xLineScreenStart.x), int(xLineScreenStart.y), int(xLineScreenEnd.x), int(xLineScreenEnd.y), 0xAAAAAAFF);
-		}
-	}
-}
-
-void DrawSphere(const Sphere& sphere, const Matrix4x4& viewProjectionMatrix, const Matrix4x4& viewprtMatrix, uint32_t color) {
-	float pi = float(M_PI);
-	const uint32_t kSbudivision = 10;//分割数
-	const float kLatEvery = pi / kSbudivision;//緯度分割1つ分の角度
-	const float kLonEvery = (2 * pi) / kSbudivision;//経度分割1つ分の角度
-	//world座標系でのa,b,cを求める
-	Vector3 a, b, c;
-	//緯度の方向に分割　-π/2～π/2
-	for (uint32_t latindex = 0; latindex < kSbudivision; ++latindex) {
-		sphere;
-		float lat = -pi / 2.0f + kLatEvery * latindex;//現在の緯度
-		//経度の方向に分割0～2π
-		for (uint32_t lonIndex = 0; lonIndex < kSbudivision; ++lonIndex) {
-			float lon = lonIndex * kLonEvery;//現在の経度
-			// ワールド座標系での頂点を求める
-			a = {
-			   (sphere.center.x + (sphere.radius * (std::cos(lat) * std::cos(lon)))),
-			   (sphere.center.y + (sphere.radius * std::sin(lat))),
-			   (sphere.center.z + (sphere.radius * (std::cos(lat) * std::sin(lon))))
-			};
-
-			b = {
-			   (sphere.center.x + (sphere.radius * (std::cos(lat + kLatEvery) * std::cos(lon)))),
-			   (sphere.center.y + (sphere.radius * std::sin(lat + kLatEvery))),
-			   (sphere.center.z + (sphere.radius * (std::cos(lat + kLatEvery) * std::sin(lon))))
-			};
-
-			c = {
-			   (sphere.center.x + (sphere.radius * (std::cos(lat) * std::cos(lon + kLonEvery)))),
-			   (sphere.center.y + (sphere.radius * std::sin(lat))),
-			   (sphere.center.z + (sphere.radius * (std::cos(lat) * std::sin(lon + kLonEvery))))
-			};
-
-			Matrix4x4 MatrixA = Matrix4x4::MakeAffineMatrix(Vector3{ 1,1,1 }, Vector3{}, a);
-			Matrix4x4 MatrixB = Matrix4x4::MakeAffineMatrix(Vector3{ 1,1,1 }, Vector3{}, b);
-			Matrix4x4 MatrixC = Matrix4x4::MakeAffineMatrix(Vector3{ 1,1,1 }, Vector3{}, c);
-			Matrix4x4 wvpMatrixA = MatrixA * viewProjectionMatrix;
-			Matrix4x4 wvpMatrixB = MatrixB * viewProjectionMatrix;
-			Matrix4x4 wvpMatrixC = MatrixC * viewProjectionMatrix;
-			Vector3 screenA = Matrix4x4::ScreenTransform(Vector3{}, wvpMatrixA, viewprtMatrix);
-			Vector3 screenB = Matrix4x4::ScreenTransform(Vector3{}, wvpMatrixB, viewprtMatrix);
-			Vector3 screenC = Matrix4x4::ScreenTransform(Vector3{}, wvpMatrixC, viewprtMatrix);
-			Novice::DrawLine(int(screenA.x), int(screenA.y), int(screenB.x), int(screenB.y), color);
-			Novice::DrawLine(int(screenA.x), int(screenA.y), int(screenC.x), int(screenC.y), color);
-		}
-	}
-}
-
-
-Vector3 Perpendicular(const Vector3& vector) {
-	if (vector.x != 0.0f || vector.y != 0.0f) {
-		return{ -vector.y,vector.x,0.0f };
-	}
-	return { 0.0f,-vector.z,vector.y };
-}
-
-bool IsCollision(const AABB& aabb1, const AABB& aabb2) {
-	if ((aabb1.min.x <= aabb2.max.x && aabb1.max.x >= aabb2.min.x) &&
-		(aabb1.min.y <= aabb2.max.y && aabb1.max.y >= aabb2.min.y) &&
-		(aabb1.min.z <= aabb2.max.z && aabb1.max.z >= aabb2.min.z)) {
-		return true;
-	}
-	else {
-		return false;
-	}
-}
-
-
-
-Vector3 Lerp(const Vector3& start, const Vector3& end, float t) {
-
-	Vector3 result;
-
-	result.x = t * start.x + (1.0f - t) * end.x;
-	result.y = t * start.y + (1.0f - t) * end.y;
-	result.z = t * start.z + (1.0f - t) * end.z;
-
-	return result;
-}
-
-
-float Clamp(float n, float min, float max) {
-	if (n > max) {
-		return max;
-	}
-	if (n < min) {
-		return min;
-	}
-	return n;
-}
-
-size_t Clamp(size_t n, size_t min, size_t max) {
-	if (n > max) {
-		return max;
-	}
-	if (n < min) {
-		return min;
-	}
-	return n;
-}
 
